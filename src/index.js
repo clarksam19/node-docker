8514d5c739ea06cd3d7836e9b8206a66e5859d4a
Sam Clark <clarksam19@gmail.com>
2021-07-20 17:18:12 -0500

    working mongo connection

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
+const config = require ('./config/config');
+const PORT = config.PORT;
+const mongoURL = `mongodb://${config.MONGO_USER}:${config.MONGO_PASSWORD}@` +
+                 `${config.MONGO_IP}:${config.MONGO_PORT}/?authSource=admin`
 const app = express();
+
+mongoose
+  .connect(mongoURL,
+    { useNewUrlParser: true , useUnifiedTopology: true }
+  )
+  .then(() => console.log('DB connection success'))
+  .catch((e) => console.log(e));
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello World!!!</h1>');
 });
-
-const PORT = process.env.PORT;
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
